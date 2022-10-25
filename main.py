b7452d7874dee95aea7208600b6f4c29e54e038e
Zalunin9 <evgeniy180602@gmail.com>
2022-10-25 20:02:24 +0300

    fegte

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+def openFile():
+  f1 = open('data.txt', 'r')
+  while True:
+      data = f1.readLine().split("-")
+      print(data)
+      if data[0] == '':
+          break
+      return daya
+      f1.close()
+   except IOError:
+        print(IOError.with_traceback() )
+   def dataMonth()
+       data2 = openFile()
+       print(data2)
+       data1 = ['01', '12', '1900']
+       print(data)
+       if int(data2[0]) < int(data1[0]):
+           data1[0] = data2[0]
+        if int(data2[1]) < int(data1[1]):
+            data2[1] = data1 [1]
+        print(data1[0], "-", data1 [1])
+
+
 try:
     f1 = open ('data.txt', 'r')
     data1 = ['31', '12', '1900']
